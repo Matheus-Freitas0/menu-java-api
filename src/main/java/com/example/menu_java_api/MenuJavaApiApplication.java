760495e481f04b76b48eca29c9f3be757bd0f71e
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-19 13:46:25 -0300

    start project

---
+++ src/main/java/com/example/menu_java_api/MenuJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.menu_java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/menu_java_api/MenuJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menu_java_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
