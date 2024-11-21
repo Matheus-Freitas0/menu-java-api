c27cf45d1e4d0639a6b53229dd21f7143ea0dbba
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-21 14:49:58 -0300

    hello world Service has been implemented

--- src/main/java/com/matheus/training_spring_boot/controller/HelloWorldController.java
+++ src/main/java/com/matheus/training_spring_boot/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.matheus.training_spring_boot.controller;
 
+import com.matheus.training_spring_boot.service.HelloWorldService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello-world")
 public class HelloWorldController {
 
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService){
+        this.helloWorldService = helloWorldService;
+    }
+
     @GetMapping
     public String helloWorld(){
-        return "Hello World";
+        return helloWorldService.helloWorld("Matheus");
     }
 
 }
---
+++ src/main/java/com/matheus/training_spring_boot/service/HelloWorldService.java
@@ -0,0 +1,13 @@
+package com.matheus.training_spring_boot.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloWorld(String name){
+        return  "Hello World" +" " +name;
+    }
+
+}
