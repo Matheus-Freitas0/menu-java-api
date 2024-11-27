a36a5e776caf5c8cd94c18d4dc37d8b0a7139985
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-26 22:28:51 -0300

    mySql structure updated

--- src/main/java/com/matheus/training_spring_boot/domain/User.java
+++ src/main/java/com/matheus/training_spring_boot/domain/User.java
@@ -1,13 +1,20 @@
 package com.matheus.training_spring_boot.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
 }
