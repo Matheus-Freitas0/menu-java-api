5754c9ebe106c49af42df79f20e079154ece3790
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-30 23:35:58 -0300

    SwaggerConfig has finished

--- src/main/java/com/matheus/training_spring_boot/config/SwaggerConfig.java
+++ src/main/java/com/matheus/training_spring_boot/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.exemplo"))
+                .apis(RequestHandlerSelectors.basePackage("com.matheus.training_spring_boot.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
