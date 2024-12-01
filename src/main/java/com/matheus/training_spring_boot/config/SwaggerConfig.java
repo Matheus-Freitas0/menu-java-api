ac0e810e150528b3b8439dfd65d8379e0d5fe348
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-12-01 13:41:27 -0300

    SwaggerConfig has finished

--- src/main/java/com/matheus/training_spring_boot/config/SwaggerConfig.java
+++ src/main/java/com/matheus/training_spring_boot/config/SwaggerConfig.java
@@ -1,21 +1,21 @@
 package com.matheus.training_spring_boot.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.matheus.training_spring_boot.controller"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Treinamento")
+                        .version("1.0.0")
+                        .description("Documentação da API de Treinamento em Spring Boot")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
